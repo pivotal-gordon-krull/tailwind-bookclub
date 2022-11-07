051ef6597ccb8c172e00395c4913b4228d0a1cd5
Gordon Krull <gkrull@vmware.com>
2022-11-07 15:14:18 +0900

    Apply Tailwind class using @apply in CSS

--- tailwind/src/App.tsx
+++ tailwind/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
+import "./App.css";
 
 function App() {
   return (
     <div className="text-center">
-      <header className="bg-app-background text-white text-app-header flex flex-col justify-center items-center min-h-screen">
+      <header className="bg-app-background red-text text-app-header flex flex-col justify-center items-center min-h-screen">
         <img src={logo} className="animate-spin-slow pointer-events-none h-40vmin" alt="logo" />
         <p className='my-6'>
           Edit <code>src/App.tsx</code> and save to reload.
